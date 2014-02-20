6a899cc15220b63c15841f3167615770b07a7730
Yuri Takhteyev <yuri@freewisdom.org>
2014-02-20 17:22:05 -0500

    More directives.

--- app/scripts/directives/task.js
+++ app/scripts/directives/task.js
@@ -3,7 +3,19 @@ angular.module('timApp')
 .directive('timTask', function(tasks) {
   return {
     replace: false,
-    scope: true,
-    template: '{{ task.title }}'
+    scope: {},
+    templateUrl: '/views/taskDirective.html',
+    link: function(scope, iElement, iAttrs) {
+      tasks.getMyTasks()
+        .then(function(myTasks) {
+          var taskNumber = parseInt(iAttrs.taskNumber);
+          console.log('Task number: ', taskNumber, ' of ', myTasks.data);
+          scope.task = myTasks.data[taskNumber];
+          scope.showButton = iAttrs.showButton;
+        })
+        .then(null, function(error) {
+          console.error(error);
+        });
+    }
   };
 });
